5f2ea8dcfbec1485f77b193ed0a0f4e59623805a
tajkirkpatrick <50558516+tajkirkpatrick@users.noreply.github.com>
2019-06-10 15:39:29 -0500

    Update SavingsCalculatorDriver.py
    Added a subtraction function, to get the days before the actual repayment start date.

--- Repayment Calculator/SavingsCalculatorDriver.py
+++ Repayment Calculator/SavingsCalculatorDriver.py
@@ -1,13 +1,14 @@
 import datetime
 
+
 class SavingsGoal:
 
     def __init__(self):
         SavingsName = "Tesla"
         TotalPaymentAmount = 0
-        Phase1_TotPaymentAmount = 0
-        Phase2_TotPaymentAmount = 0
-        Phase3_TotPaymentAmount = 0
+        Phase1Tot_TotalPaymentAmount = 0
+        Phase2Tot_TotalPaymentAmount = 0
+        Phase3Tot_TotalPaymentAmount = 0
 
     def run(self):
 
@@ -23,27 +24,27 @@ class SavingsGoal:
             print("That's not an int!")
             print("No.. input string is not an Integer. It's a string")
 
-        Phase1Tot_TotalPaymentAmount = self.TotalPaymentAmount*.33
-        Phase2Tot_TotalPaymentAmount = self.TotalPaymentAmount*.50
-        Phase3Tot_TotalPaymentAmount = self.TotalPaymentAmount*.17
+        self.Phase1Tot_TotalPaymentAmount = self.TotalPaymentAmount*.33
+        self.Phase2Tot_TotalPaymentAmount = self.TotalPaymentAmount*.50
+        self.Phase3Tot_TotalPaymentAmount = self.TotalPaymentAmount*.17
 
-        Phase1_Payments.append(float(Phase1Tot_TotalPaymentAmount)*.2)
-        Phase1_Payments.append(float(Phase1Tot_TotalPaymentAmount)*.2)
-        Phase1_Payments.append(float(Phase1Tot_TotalPaymentAmount)*.2)
-        Phase1_Payments.append(float(Phase1Tot_TotalPaymentAmount)*.15)
-        Phase1_Payments.append(float(Phase1Tot_TotalPaymentAmount)*.25)
+        Phase1_Payments.append(float(self.Phase1Tot_TotalPaymentAmount)*.2)
+        Phase1_Payments.append(float(self.Phase1Tot_TotalPaymentAmount)*.2)
+        Phase1_Payments.append(float(self.Phase1Tot_TotalPaymentAmount)*.2)
+        Phase1_Payments.append(float(self.Phase1Tot_TotalPaymentAmount)*.15)
+        Phase1_Payments.append(float(self.Phase1Tot_TotalPaymentAmount)*.25)
 
-        Phase2_Payments.append(float(Phase2Tot_TotalPaymentAmount)*.10)
-        Phase2_Payments.append(float(Phase2Tot_TotalPaymentAmount)*.15)
-        Phase2_Payments.append(float(Phase2Tot_TotalPaymentAmount)*.25)
-        Phase2_Payments.append(float(Phase2Tot_TotalPaymentAmount)*.30)
-        Phase2_Payments.append(float(Phase2Tot_TotalPaymentAmount)*.20)
+        Phase2_Payments.append(float(self.Phase2Tot_TotalPaymentAmount)*.10)
+        Phase2_Payments.append(float(self.Phase2Tot_TotalPaymentAmount)*.15)
+        Phase2_Payments.append(float(self.Phase2Tot_TotalPaymentAmount)*.25)
+        Phase2_Payments.append(float(self.Phase2Tot_TotalPaymentAmount)*.30)
+        Phase2_Payments.append(float(self.Phase2Tot_TotalPaymentAmount)*.20)
 
-        Phase3_Payments.append(float(Phase3Tot_TotalPaymentAmount)*.30)
-        Phase3_Payments.append(float(Phase3Tot_TotalPaymentAmount)*.25)
-        Phase3_Payments.append(float(Phase3Tot_TotalPaymentAmount)*.20)
-        Phase3_Payments.append(float(Phase3Tot_TotalPaymentAmount)*.15)
-        Phase3_Payments.append(float(Phase3Tot_TotalPaymentAmount)*.10)
+        Phase3_Payments.append(float(self.Phase3Tot_TotalPaymentAmount)*.30)
+        Phase3_Payments.append(float(self.Phase3Tot_TotalPaymentAmount)*.25)
+        Phase3_Payments.append(float(self.Phase3Tot_TotalPaymentAmount)*.20)
+        Phase3_Payments.append(float(self.Phase3Tot_TotalPaymentAmount)*.15)
+        Phase3_Payments.append(float(self.Phase3Tot_TotalPaymentAmount)*.10)
 
         # sum_payments = 0
 
@@ -53,9 +54,12 @@ class SavingsGoal:
 
         #  TODO: Take the Start and End Date from the user and divide the days between the two dates into 3 phase increments with 15 payments. So divide the total amount of days by 3, and divide the total amount of days by 15
 
+        # August 25, 2018 - Start Date of Loan Repayment
         start_date = datetime.datetime(2019, 8, 25)
         diff = start_date - datetime.datetime.now()
-        print(diff)
+        print(
+            "There are %s total days between now and when you plan to begin" % diff.days)
+        print(str(diff.days/3) + " days per phase")
 
         exit
 
