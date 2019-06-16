3413b4ab54e16df9aeaf28a18dea572c02297a68
tajkirkpatrick <tkirk18@hotmail.com>
2019-06-15 21:24:50 -0500

    Updated SavingsGoal Class, SavingsCalculatorDriver

--- Repayment Calculator/SavingsCalculatorDriver.py
+++ Repayment Calculator/SavingsCalculatorDriver.py
@@ -1,167 +1,146 @@
-import datetime
+from SavingsGoal import SavingsGoal
 
+Tesla = SavingsGoal("Tesla", 50000)
+Tesla.setTotalAmount(100)
+Tesla.getAmount()
+    #     Phase1_Payments = []
+    #     Phase2_Payments = []
+    #     Phase3_Payments = []
+    #     Phase1_Dates = []
+    #     Phase2_Dates = []
+    #     Phase3_Dates = []
+    #     userYear_date = 0
+    #     userMonth_date = 0
+    #     userDay_date = 0
 
-class SavingsGoal:
+    #     self.TotalPaymentAmount = 500.00
 
-    def __init__(self):
-        SavingsName = "Tesla"
-        TotalPaymentAmount = 0
-        Phase1Tot_TotalPaymentAmount = 0
-        Phase2Tot_TotalPaymentAmount = 0
-        Phase3Tot_TotalPaymentAmount = 0
+    #     self.DivyItUp(self.TotalPaymentAmount, Phase1_Payments,
+    #                   Phase2_Payments, Phase3_Payments)
 
-    def run(self):
+    #     # sum_payments = 0
 
-        Phase1_Payments = []
-        Phase2_Payments = []
-        Phase3_Payments = []
-        Phase1_Dates = []
-        Phase2_Dates = []
-        Phase3_Dates = []
-        userYear_date = 0
-        userMonth_date = 0
-        userDay_date = 0
+    #     # for payment in Phase1_Payments[0:3]:
+    #     #     sum_payments += payment
+    #     #     print(sum_payments)
 
-        self.TotalPaymentAmount = 500.00
-        # try:
-        #     self.TotalPaymentAmount = float(self.TotalPaymentAmount)
-        #     print("Input number value is: ", self.TotalPaymentAmount)
-        # except ValueError:
-        #     print("That's not an int!")
-        #     print("No.. input string is not an Integer. It's a string")
+    #     #  TODO: Take the Start and End Date from the user and divide the days between the two dates into 3 phase increments with 15 payments. So divide the total amount of days by 3, and divide the total amount of days by 15
 
-        self.DivyItUp(self.TotalPaymentAmount, Phase1_Payments,
-                      Phase2_Payments, Phase3_Payments)
+    #     # August 25, 2018 - Start Date of Loan Repayment
 
-        # sum_payments = 0
+    #     date_now = datetime.datetime.now()
+    #     print("Today's Date is: " + str(date_now.strftime("%A" + " " + "%x")))
 
-        # for payment in Phase1_Payments[0:3]:
-        #     sum_payments += payment
-        #     print(sum_payments)
+    #     mininmumStart_date = date_now + datetime.timedelta(days=92)
+    #     print("Minimum Payment Plan Start Date: " +
+    #           str(mininmumStart_date.strftime("%x")))
 
-        #  TODO: Take the Start and End Date from the user and divide the days between the two dates into 3 phase increments with 15 payments. So divide the total amount of days by 3, and divide the total amount of days by 15
+    # #    try:
+    # #         userYear_date = int(input("Enter Year: "))
+    # #     except (UnboundLocalError, ValueError) as e:
+    # #         print("Invalid Input, Error: %s" % e)
 
-        # August 25, 2018 - Start Date of Loan Repayment
+    # #     try:
+    # #         userMonth_date = int(input("Enter Month: "))
+    # #     except (UnboundLocalError, ValueError) as e:
+    # #         print("Invalid Input, Error: %s" % e)
 
-        date_now = datetime.datetime.now()
-        print("Today's Date is: " + str(date_now.strftime("%A" + " " + "%x")))
+    # #     try:
+    # #         userDay_date = int(input("Enter Day: "))
+    # #     except (UnboundLocalError, ValueError) as e:
+    # #         print("Invalid Input, Error: %s" % e)
 
-        mininmumStart_date = date_now + datetime.timedelta(days=92)
-        print("Minimum Payment Plan Start Date: " +
-              str(mininmumStart_date.strftime("%x")))
+    #     # Testing Purposes
+    #     userStart_date = datetime.datetime(2019, 9, 25)
+    #     userEnd_date = datetime.datetime(2026, 10, 26)
 
-    #    try:
-    #         userYear_date = int(input("Enter Year: "))
-    #     except (UnboundLocalError, ValueError) as e:
-    #         print("Invalid Input, Error: %s" % e)
+    #     print("The User Start Date is: " + str(userStart_date.strftime("%x")))
+    #     print("The User End Date is: " + str(userEnd_date.strftime("%x")))
 
-    #     try:
-    #         userMonth_date = int(input("Enter Month: "))
-    #     except (UnboundLocalError, ValueError) as e:
-    #         print("Invalid Input, Error: %s" % e)
+    #         # less than means "in the past", this asks, does the minimum start date come before the NOW date? result: false
+    #     if userStart_date < mininmumStart_date:
+    #         print("Sorry, your user start date is invalid!")
+    #     else:
+    #         print("Acceptable user start date!")
 
-    #     try:
-    #         userDay_date = int(input("Enter Day: "))
-    #     except (UnboundLocalError, ValueError) as e:
-    #         print("Invalid Input, Error: %s" % e)
-
-        # Testing Purposes
-        userStart_date = datetime.datetime(2019, 9, 25)
-        userEnd_date = datetime.datetime(2026, 10, 26)
-
-        print("The User Start Date is: " + str(userStart_date.strftime("%x")))
-        print("The User End Date is: " + str(userEnd_date.strftime("%x")))
-
-            # less than means "in the past", this asks, does the minimum start date come before the NOW date? result: false
-        if userStart_date < mininmumStart_date:
-            print("Sorry, your user start date is invalid!")
-        else:
-            print("Acceptable user start date!")
-
-        Tot_RepaymentDays = userEnd_date - userStart_date
+    #     Tot_RepaymentDays = userEnd_date - userStart_date
         
-        DaysPerPhase_RepaymentDays = round(float(Tot_RepaymentDays.days/3))
-        DaysBetweenPayments_RepaymentDays = (round(float(Tot_RepaymentDays.days/15)))
+    #     DaysPerPhase_RepaymentDays = round(float(Tot_RepaymentDays.days/3))
+    #     DaysBetweenPayments_RepaymentDays = (round(float(Tot_RepaymentDays.days/15)))
 
-        if int(DaysPerPhase_RepaymentDays)%2 != 0:
-            round(float(DaysPerPhase_RepaymentDays))
+    #     if int(DaysPerPhase_RepaymentDays)%2 != 0:
+    #         round(float(DaysPerPhase_RepaymentDays))
 
-        print("Amount of Days between Start & End: "+ str(Tot_RepaymentDays.days))
-        print("Initial Days per Phase = " + str(DaysPerPhase_RepaymentDays))
+    #     print("Amount of Days between Start & End: "+ str(Tot_RepaymentDays.days))
+    #     print("Initial Days per Phase = " + str(DaysPerPhase_RepaymentDays))
 
-        print("Initial Days between Phase Payments = " + str(DaysBetweenPayments_RepaymentDays))
+    #     print("Initial Days between Phase Payments = " + str(DaysBetweenPayments_RepaymentDays))
 
-        # * Phase Ranges (Display the date ranges)
-        Phase1End_date = userStart_date + datetime.timedelta(DaysPerPhase_RepaymentDays)
-        Phase2End_date = Phase1End_date + datetime.timedelta(DaysPerPhase_RepaymentDays)
-        Phase3End_date = Phase2End_date + datetime.timedelta(DaysPerPhase_RepaymentDays)
+    #     # * Phase Ranges (Display the date ranges)
+    #     Phase1End_date = userStart_date + datetime.timedelta(DaysPerPhase_RepaymentDays)
+    #     Phase2End_date = Phase1End_date + datetime.timedelta(DaysPerPhase_RepaymentDays)
+    #     Phase3End_date = Phase2End_date + datetime.timedelta(DaysPerPhase_RepaymentDays)
 
-        print("\nThe User Start Date is: " + str(userStart_date.strftime("%x")))
-        print("Phase 1 Ends " + str(Phase1End_date.strftime("%x")))
-        print("Phase 2 Ends " + str(Phase2End_date.strftime("%x")))
-        print("Phase 3 Ends " + str(Phase3End_date.strftime("%x")))
-        print("The End Date is: " + str(userEnd_date.strftime("%x")))
+    #     print("\nThe User Start Date is: " + str(userStart_date.strftime("%x")))
+    #     print("Phase 1 Ends " + str(Phase1End_date.strftime("%x")))
+    #     print("Phase 2 Ends " + str(Phase2End_date.strftime("%x")))
+    #     print("Phase 3 Ends " + str(Phase3End_date.strftime("%x")))
+    #     print("The End Date is: " + str(userEnd_date.strftime("%x")))
 
-        # if userEnd_date < Phase3End_date:
-        #     print("\tThe User's End Date conflicts with the payment schedule: Rescheduling End Date")
-        #     print("\tThe User End Date was: " + str(userEnd_date.strftime("%x")))
-        #     print("\tPhase 3 was scheduled to end: " + str(Phase3End_date.strftime("%x")))
-        #     userEnd_date = Phase3End_date
-        #     userEnd_date += datetime.timedelta(7)
-        #     print("The New End Date is: " + str(userEnd_date.strftime("%x")))
+    #     # if userEnd_date < Phase3End_date:
+    #     #     print("\tThe User's End Date conflicts with the payment schedule: Rescheduling End Date")
+    #     #     print("\tThe User End Date was: " + str(userEnd_date.strftime("%x")))
+    #     #     print("\tPhase 3 was scheduled to end: " + str(Phase3End_date.strftime("%x")))
+    #     #     userEnd_date = Phase3End_date
+    #     #     userEnd_date += datetime.timedelta(7)
+    #     #     print("The New End Date is: " + str(userEnd_date.strftime("%x")))
 
-        # * Phase 1 Payment Date Ranges
+    #     # * Phase 1 Payment Date Ranges
 
-
-
-        print("\n\nBreakdown!\nPhase 1:")
+    #     print("\n\nBreakdown!\nPhase 1:")
 
 
-    # TODO: Create a breakdown of when payments are due including the dates
+    # # TODO: Create a breakdown of when payments are due including the dates
 
 
-    def Phase3_DivyItUp(self, Phase3_Payments):
-        Phase3_Payments.append(float(self.Phase3Tot_TotalPaymentAmount)*.30)
-        Phase3_Payments.append(float(self.Phase3Tot_TotalPaymentAmount)*.25)
-        Phase3_Payments.append(float(self.Phase3Tot_TotalPaymentAmount)*.20)
-        Phase3_Payments.append(float(self.Phase3Tot_TotalPaymentAmount)*.15)
-        Phase3_Payments.append(float(self.Phase3Tot_TotalPaymentAmount)*.10)
+    # def Phase3_DivyItUp(self, Phase3_Payments):
+    #     Phase3_Payments.append(float(self.Phase3Tot_TotalPaymentAmount)*.30)
+    #     Phase3_Payments.append(float(self.Phase3Tot_TotalPaymentAmount)*.25)
+    #     Phase3_Payments.append(float(self.Phase3Tot_TotalPaymentAmount)*.20)
+    #     Phase3_Payments.append(float(self.Phase3Tot_TotalPaymentAmount)*.15)
+    #     Phase3_Payments.append(float(self.Phase3Tot_TotalPaymentAmount)*.10)
 
-    def Phase2_DivyItUp(self, Phase2_Payments):
-        Phase2_Payments.append(float(self.Phase2Tot_TotalPaymentAmount)*.10)
-        Phase2_Payments.append(float(self.Phase2Tot_TotalPaymentAmount)*.15)
-        Phase2_Payments.append(float(self.Phase2Tot_TotalPaymentAmount)*.25)
-        Phase2_Payments.append(float(self.Phase2Tot_TotalPaymentAmount)*.30)
-        Phase2_Payments.append(float(self.Phase2Tot_TotalPaymentAmount)*.20)
+    # def Phase2_DivyItUp(self, Phase2_Payments):
+    #     Phase2_Payments.append(float(self.Phase2Tot_TotalPaymentAmount)*.10)
+    #     Phase2_Payments.append(float(self.Phase2Tot_TotalPaymentAmount)*.15)
+    #     Phase2_Payments.append(float(self.Phase2Tot_TotalPaymentAmount)*.25)
+    #     Phase2_Payments.append(float(self.Phase2Tot_TotalPaymentAmount)*.30)
+    #     Phase2_Payments.append(float(self.Phase2Tot_TotalPaymentAmount)*.20)
 
-    def Phase1_DivyItUp(self, Phase1_Payments):
-        Phase1_Payments.append(float(self.Phase1Tot_TotalPaymentAmount)*.2)
-        Phase1_Payments.append(float(self.Phase1Tot_TotalPaymentAmount)*.2)
-        Phase1_Payments.append(float(self.Phase1Tot_TotalPaymentAmount)*.2)
-        Phase1_Payments.append(float(self.Phase1Tot_TotalPaymentAmount)*.15)
-        Phase1_Payments.append(float(self.Phase1Tot_TotalPaymentAmount)*.25)
+    # def Phase1_DivyItUp(self, Phase1_Payments):
+    #     Phase1_Payments.append(float(self.Phase1Tot_TotalPaymentAmount)*.2)
+    #     Phase1_Payments.append(float(self.Phase1Tot_TotalPaymentAmount)*.2)
+    #     Phase1_Payments.append(float(self.Phase1Tot_TotalPaymentAmount)*.2)
+    #     Phase1_Payments.append(float(self.Phase1Tot_TotalPaymentAmount)*.15)
+    #     Phase1_Payments.append(float(self.Phase1Tot_TotalPaymentAmount)*.25)
 
-    def DivyItUp(self, TotalPaymentAmount, Phase1_Payments, Phase2_Payments, Phase3_Payments):
-        TotalPaymentAmount = self.TotalPaymentAmount
+    # def DivyItUp(self, TotalPaymentAmount, Phase1_Payments, Phase2_Payments, Phase3_Payments):
+    #     TotalPaymentAmount = self.TotalPaymentAmount
 
-        self.Phase1Tot_TotalPaymentAmount = TotalPaymentAmount*.33
-        self.Phase2Tot_TotalPaymentAmount = TotalPaymentAmount*.50
-        self.Phase3Tot_TotalPaymentAmount = TotalPaymentAmount*.17
+    #     self.Phase1Tot_TotalPaymentAmount = TotalPaymentAmount*.33
+    #     self.Phase2Tot_TotalPaymentAmount = TotalPaymentAmount*.50
+    #     self.Phase3Tot_TotalPaymentAmount = TotalPaymentAmount*.17
 
-        self.Phase1_DivyItUp(Phase1_Payments)
-        self.Phase2_DivyItUp(Phase2_Payments)
-        self.Phase3_DivyItUp(Phase3_Payments)
+    #     self.Phase1_DivyItUp(Phase1_Payments)
+    #     self.Phase2_DivyItUp(Phase2_Payments)
+    #     self.Phase3_DivyItUp(Phase3_Payments)
 
-        # diff = userStart_date - date_now
-        # if diff.days < 91:
-        #     print(
-        #         "You must select a date more than 3 Months (91 days) Away\nYours is: %s" % diff.days)
+    #     # diff = userStart_date - date_now
+    #     # if diff.days < 91:
+    #     #     print(
+    #     #         "You must select a date more than 3 Months (91 days) Away\nYours is: %s" % diff.days)
 
-        # diff = start_date - datetime.datetime.now()
-        # print(
-        #     "There are %s total days between now and when you plan to begin" % diff.days)
-        # print(str(diff.days/3) + " days per phase")
-
-
-Tesla = SavingsGoal()
-Tesla.run()
+    #     # diff = start_date - datetime.datetime.now()
+    #     # print(
+    #     #     "There are %s total days between now and when you plan to begin" % diff.days)
+    #     # print(str(diff.days/3) + " days per phase")
--- Repayment Calculator/SavingsGoal.py
+++ Repayment Calculator/SavingsGoal.py
@@ -1,14 +1,76 @@
 import datetime
 
 
-class SavingsGoal:
+class SavingsGoal(object):
 
-    def __init__(self):
-        SavingsName = "Tesla"
-        TotalPaymentAmount = 0
-        Phase1Tot_TotalPaymentAmount = 0
-        Phase2Tot_TotalPaymentAmount = 0
-        Phase3Tot_TotalPaymentAmount = 0
+    def __init__(self, name, amount):
+        self.SavingsName = name
+        self.TotalPaymentAmount = amount
+        self.Phase1Tot_TotalPaymentAmount = self.TotalPaymentAmount*.33
+        self.Phase2Tot_TotalPaymentAmount = self.TotalPaymentAmount*.50
+        self.Phase3Tot_TotalPaymentAmount = self.TotalPaymentAmount*.17
+        validStartDate = False
+        self.userStartDate = None
 
-   def setPaymentAmount(self, TotalPaymentAmount):
-        self.TotalPaymentAmount = TotalPaymentAmount
+    def setTotalAmount(self, newAmount):
+        self.TotalPaymentAmount = newAmount
+
+    def setStartDate(self):
+        monthsWithout31 = [2, 4, 6, 9, 11]
+        userYear = None
+        userMonth = None
+        userDay = None
+
+        # * Take the user's year, it can't be less than the current year
+        user_input = input("What is the Year of the start date?\n")
+        try:
+            val = int(user_input)
+            if len(user_input) != 4 or val < datetime.datetime.now().year:
+                raise ValueError
+            else:
+                userYear = val
+                print("User Year Captured (NOT Saved!)\n")
+        except ValueError as error:
+            print('Your value is not an acceptable year\nNo values were saved.')
+            return 1
+
+        # * Month
+        user_input = input("What is the Month of the start date?\n")
+        try:
+            val = int(user_input)
+            if val < 1 or val > 12:
+                raise ValueError
+
+            if (userYear == datetime.datetime.now().year and val <= datetime.datetime.now().month):
+                raise ValueError
+            else:
+                userMonth = val
+                print("User Month Captured (NOT Saved Yet!)\n")
+        except ValueError as error:
+            print('Your value is not an acceptable month')
+            return 2
+
+        # * Day
+        user_input = input("What is the Day of the start date?\n")
+        try:
+            val = int(user_input)
+            if val < 1 or val > 31:
+                raise ValueError
+
+            if val == 31 and userMonth in monthsWithout31:
+                raise ValueError
+            else:
+                userDay = val
+                print("User Day Captured (Saving Soon)\n")
+        except ValueError as error:
+            print('Your value is not an acceptable month\day combination')
+            return 3
+
+        self.userStartDate = datetime.datetime(userYear, userMonth, userDay)
+        validStartDate = True
+        print("User Start Date Saved!")
+        return 0
+
+    def getAmount(self):
+        print(self.TotalPaymentAmount)
+        return self.TotalPaymentAmount
